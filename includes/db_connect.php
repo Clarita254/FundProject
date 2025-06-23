c630bbd6dd1dc8309228bf6832aeb79fb1f569af
Clarita254 <clara.waithaka@strathmore.edu>
2025-06-23 11:43:16 +0300

    Created connection to edufund_db database

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,17 +1,18 @@
 <?php
 
-session_start();
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db_name="Edufund_db";
+$db_name="edufund_db";
 
 // Create connection
-$conn = new mysqli($host, $username, $password,$db_name);
+$conn = new mysqli($servername, $username, $password,$db_name);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+echo "Connected successfully";
 ?>
+
